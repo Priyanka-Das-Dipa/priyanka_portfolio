66ede47692349d5d8c4a5bf804ab57ebebf54c83
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-03-15 20:48:39 +0600

    two projects added

--- src/components/MySkill.jsx
+++ src/components/MySkill.jsx
@@ -17,193 +17,206 @@ import mongoose from "../../public/image/mongoose.png";
 // bg-gradient-to-r from-[#059bff62] via-[#6549d5] to-[#e33fa1]
 const MySkill = () => {
   return (
-    <div id="skill" className="mb-10 px-5 md:px-12 ">
-        <div className="flex gap-3 items-start" >
-            <span className="w-[10px] h-[40px] bg-gradient-to-r from-[#059bff62] via-[#6549d5] to-[#e33fa1]"></span>
-            <span className="text-white"> I am comfortable with this technology&apos;s <br className="hidden lg:block"/><span>Committed to learning new technologies and staying up-to-date in my field.</span></span>
+    <section
+      id="skill"
+      className=" px-5 md:px-12 bg-gradient-to-b from-[#0c0c1d] to-[#111132]"
+    >
+      <div className="py-20">
+        <div className="flex gap-3 items-start">
+          <span className="w-[10px] h-[40px] bg-gradient-to-r from-[#059bff62] via-[#6549d5] to-[#e33fa1]"></span>
+          <span className="text-white">
+            {" "}
+            I am comfortable with this technology&apos;s{" "}
+            <br className="hidden lg:block" />
+            <span>
+              Committed to learning new technologies and staying up-to-date in
+              my field.
+            </span>
+          </span>
         </div>
-      <h1 className="text-2xl font-bold text-slate-200 py-8">
-        Frontend Technology{" "}
-      </h1>
-      <div className="flex flex-wrap items-center gap-5 md:gap-8">
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={html}
-              className="w-6 xl:w-8 h-6 xl:h-8"
-              alt="tailwind"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">HTML</p>
+        <h1 className="text-2xl font-bold text-slate-200 py-8">
+          Frontend Technology{" "}
+        </h1>
+        <div className="flex flex-wrap items-center gap-5 md:gap-8">
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={html}
+                className="w-6 xl:w-8 h-6 xl:h-8"
+                alt="tailwind"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">HTML</p>
+            </div>
+          </div>
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={css}
+                className="w-6 xl:w-8 h-6 xl:h-8"
+                alt="tailwind"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">CSS</p>
+            </div>
+          </div>
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={tailwind}
+                className="w-6 xl:w-8 h-6 xl:h-8"
+                alt="tailwind"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">Tailwind</p>
+            </div>
+          </div>
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={JavaScript}
+                className="w-6 xl:w-8 h-6 xl:h-8"
+                alt="tailwind"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">JavaScript</p>
+            </div>
+          </div>
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={react}
+                className="w-6 xl:w-8 h-6 xl:h-8"
+                alt="react"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">React js</p>
+            </div>
+          </div>
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={reactRouter}
+                className="w-6 xl:w-8 h-6 xl:h-8"
+                alt="reactRouter"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">React Router</p>
+            </div>
+          </div>
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={next}
+                className="w-6 xl:w-8 h-6 xl:h-8"
+                alt="next"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">Next js</p>
+            </div>
           </div>
         </div>
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={css}
-              className="w-6 xl:w-8 h-6 xl:h-8"
-              alt="tailwind"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">CSS</p>
+        <h1 className="text-2xl font-bold text-slate-200 py-8">
+          Backend Technology{" "}
+        </h1>
+        <div className="flex flex-wrap items-center gap-5 md:gap-8">
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={firebase}
+                className="w-6 xl:w-8 h-6 xl:h-8"
+                alt="firebase"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">Firebase</p>
+            </div>
+          </div>
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={node}
+                className="w-6 xl:w-8 h-6 xl:h-8"
+                alt="node"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">Node js</p>
+            </div>
+          </div>
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={express}
+                className="w-6 xl:w-8 h-6 xl:h-8"
+                alt="express"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">Express js</p>
+            </div>
+          </div>
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={MongoDB}
+                className="w-6 xl:w-8 h-6 xl:h-8"
+                alt="mongodb"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">MongoDB</p>
+            </div>
+          </div>
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={mongoose}
+                className="w-6 xl:w-8 h-6 xl:h-8 bg-cover"
+                alt="mongoose"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">Mongoose</p>
+            </div>
           </div>
         </div>
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={tailwind}
-              className="w-6 xl:w-8 h-6 xl:h-8"
-              alt="tailwind"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">Tailwind</p>
+        <h1 className="text-2xl font-bold text-slate-200 py-8">Tools</h1>
+        <div className="flex flex-wrap items-center gap-5 md:gap-8">
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={git}
+                className="w-6 xl:w-8 h-6 xl:h-8"
+                alt="mongodb"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">Git</p>
+            </div>
           </div>
-        </div>
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={JavaScript}
-              className="w-6 xl:w-8 h-6 xl:h-8"
-              alt="tailwind"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">JavaScript</p>
-          </div>
-        </div>
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={react}
-              className="w-6 xl:w-8 h-6 xl:h-8"
-              alt="react"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">React js</p>
-          </div>
-        </div>
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={reactRouter}
-              className="w-6 xl:w-8 h-6 xl:h-8"
-              alt="reactRouter"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">React Router</p>
-          </div>
-        </div>
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={next}
-              className="w-6 xl:w-8 h-6 xl:h-8"
-              alt="next"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">Next js</p>
+          <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
+            <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
+              <Image
+                src={figma}
+                className="w-6 xl:w-8 h-6 xl:h-8"
+                alt="figma"
+                width={10}
+                height={10}
+              />
+              <p className="font-semibold text-xl text-white">Figma</p>
+            </div>
           </div>
         </div>
       </div>
-      <h1 className="text-2xl font-bold text-slate-200 py-8">
-        Backend Technology{" "}
-      </h1>
-      <div className="flex flex-wrap items-center gap-5 md:gap-8">
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={firebase}
-              className="w-6 xl:w-8 h-6 xl:h-8"
-              alt="firebase"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">Firebase</p>
-          </div>
-        </div>
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={node}
-              className="w-6 xl:w-8 h-6 xl:h-8"
-              alt="node"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">Node js</p>
-          </div>
-        </div>
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={express}
-              className="w-6 xl:w-8 h-6 xl:h-8"
-              alt="express"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">Express js</p>
-          </div>
-        </div>
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={MongoDB}
-              className="w-6 xl:w-8 h-6 xl:h-8"
-              alt="mongodb"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">MongoDB</p>
-          </div>
-        </div>
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={mongoose}
-              className="w-6 xl:w-8 h-6 xl:h-8 bg-cover"
-              alt="mongoose"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">Mongoose</p>
-          </div>
-        </div>
-      </div>
-      <h1 className="text-2xl font-bold text-slate-200 py-8">Tools</h1>
-      <div className="flex flex-wrap items-center gap-5 md:gap-8">
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={git}
-              className="w-6 xl:w-8 h-6 xl:h-8"
-              alt="mongodb"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">Git</p>
-          </div>
-        </div>
-        <div className="bg-gradient-to-r from-[#059dff]/10 via-[#6549d5]/10 to-[#e33fa1]/10 rounded-md">
-          <div className="flex justify-center items-center gap-3 px-2 py-1 sm:px-8 sm:py-3 h-full w-full">
-            <Image
-              src={figma}
-              className="w-6 xl:w-8 h-6 xl:h-8"
-              alt="figma"
-              width={10}
-              height={10}
-            />
-            <p className="font-semibold text-xl text-white">Figma</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    </section>
   );
 };
 
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,9 +3,13 @@ import "./imageCss.css";
 import Link from "next/link";
 const Projects = () => {
   return (
-    <section id="project" className="bg-gradient-to-b from-[#0c0c1d] to-[#111132] px-5 md:px-12 ">
-      <div  className="mt-28 ">
-        <div className="md:grid md:grid-cols-1 xl:grid-cols-2 gap-2 items-start py-8 px-4 xl:gap-5 sm:py-16 xl:py-16">
+    <section
+      id="project"
+      className="bg-gradient-to-b from-[#0c0c1d] to-[#111132] px-5 md:px-12 "
+    >
+      <div className="pt-28 pb-16">
+        {/* project 1 */}
+        <div className="md:grid md:grid-cols-1 xl:grid-cols-2 gap-2 items-start py-8 px-4 xl:gap-5 sm:py-16 xl:py-20 ">
           <div className="">
             <div className="box1"></div>
           </div>
@@ -13,7 +17,7 @@ const Projects = () => {
             <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-pink-700 text-4xl py-2 font-extrabold">
               Library Service Management
             </h1>
-            <div className="md:flex flex-col gap-2 text-left pb-2">
+            <div className="flex md:flex-row flex-col gap-2 text-left pb-2">
               <p className="bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-indigo-400 text-md font-bold">
                 Technology :
               </p>
@@ -23,41 +27,143 @@ const Projects = () => {
                 </h3>
               </div>
             </div>
+            <p className="text-slate-200">
+            Introducing our innovative Library Service Management System – a comprehensive web-based platform designed to revolutionise the way users interact with library services. Our feature-rich website ensures a seamless experience for both administrators and members, offering a plethora of functionalities to streamline the subscription and management of books.
+            </p>
+            <div className="text-white">
+              <p className="bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-indigo-600 text-md font-bold">
+                Project Feature&apos;s :
+              </p>
+              <ul className="list-disc pl-5 space-y-1">
+                <li>
+                  <span className="font-bold">
+                    Subscription Management System:{" "}
+                  </span>
+                  Showcase your implementation of a subscription management
+                  system using Stripe integration, allowing users to easily
+                  subscribe to your service and make payments through the
+                  pricing route.
+                </li>
+                <li>
+                  <span className="font-bold">
+                    Search and Pagination Functionality:{" "}
+                  </span>
+                  Highlight your implementation of search functionality and
+                  pagination for the all books page. Search for books using
+                  backend search capabilities and navigate through multiple
+                  pages of search results using pagination.
+                </li>
+                <li>
+                  <span className="font-bold">Writer Management:{" "}</span>
+                  Showcase the functionality for managing writers on your
+                  website, including the ability for users to view all writers
+                  and access detailed information about specific writers via the
+                  writers page.
+                </li>
+                <li>
+                  <span className="font-bold">
+                    User Management in Admin Dashboard:{" "}
+                  </span>
+                  Emphasize the user management features available in the admin
+                  dashboard, such as viewing a list of users, adding new users,
+                  and deleting existing users. Highlight any additional
+                  functionality related to user management, such as user roles
+                  and permissions, account settings, or user activity tracking,
+                  showcasing your ability to create robust admin interfaces for
+                  managing user-related tasks efficiently.
+                </li>
+              </ul>
+            </div>
+            <div className="flex gap-7 mt-5 ">
+              <Link href="https://library-service-management-system.vercel.app/">
+                <button className="border text-white px-5 py-1 rounded-md ">
+                  Live
+                </button>
+              </Link>
+
+              <Link href="https://github.com/codefusion6/library-service-management-system">
+                <button className="border text-white px-5 py-1 rounded-md">
+                  Github
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* project 2 */}
+        <div className="md:grid md:grid-cols-1 xl:grid-cols-2 gap-2 items-start py-8 px-4 xl:gap-5 sm:py-16 xl:py-20">
+          <div className="">
+            <div className="box2"></div>
+          </div>
+          <div className="text-left space-y-3">
+            <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-pink-700 text-4xl py-2 font-extrabold">
+              Technology_And_Electronic
+            </h1>
+            <div className="flex flex-col md:flex-row gap-2 text-left pb-2">
+              <p className="bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-indigo-400 text-md font-bold">
+                Technology:
+              </p>
+              <div className="text-white">
+                <h3 className="text-base font-medium">
+                  React, React Router DOM, Firebase, Node js, Express, MongoDB,
+                  JWT, Mamba UI
+                </h3>
+              </div>
+            </div>
+            <p className="text-slate-200">
+              This is a web based online Mobile project which is mainly about
+              the brands of the mobile phones. There are six brand of mobile
+              phones. Each category have own brand of mobile, laptop etc. Anyone
+              can easily buy phones from the website.
+            </p>
             <div className="text-white">
               <p className="bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-indigo-600 text-md font-bold">
                 Project Feature&apos;s :
               </p>
               <ul className="list-disc pl-5 space-y-2">
                 <li>
-                  Fully integrated lending page with navbar, banner, recommended
-                  and featured books, author section, and footer.
+                  <span className="font-bold">User-Friendly Navigation: </span>
+                  Implement a user-friendly navigation system that allows users
+                  to easily browse through the six different mobile phone brands
+                  and their respective products.
                 </li>
                 <li>
-                  Stripe integration for subscription payments in the pricing
-                  route.
+                  <span className="font-bold">E-commerce Functionality: </span>
+                  Showcase your implementation of e-commerce features such as
+                  adding products to a shopping cart, viewing product details,
+                  and completing purchases securely.
                 </li>
                 <li>
-                  Backend pagination and search functionality for the all books
-                  page.
+                  <span className="font-bold">
+                    User Authentication and Authorization:
+                  </span>
+                  Detail your implementation of user authentication and
+                  authorization features to ensure secure access to
+                  user-specific functionalities.
                 </li>
-                <li>Dynamic display of author&apos;s books on details page.</li>
                 <li>
-                  Admin dashboard for managing users, books, writers, book
-                  requests, and membership requests.
-                </li>
-                <li>
-                  Implemented secure user authentication system with login
-                  functionality, save user&apos;s info in database.
+                  <span className="font-bold">Responsive Design:</span>
+                  Emphasize your use of responsive web design techniques to
+                  ensure the website is accessible and functional across various
+                  devices and screen sizes.
                 </li>
               </ul>
             </div>
-            <div className="flex gap-7 mt-5">
-              <Link href="https://library-service-management-system.vercel.app/">
-                <button className="border text-white px-5 ">Live</button>
+            <div className="flex gap-3 mt-5">
+              <Link href="https://technology-and-electroni-8514d.web.app/">
+                <button className="border text-white px-5 py-1 rounded-md">
+                  Live
+                </button>
               </Link>
 
-              <Link href="https://github.com/codefusion6/library-service-management-system">
-                <button className="border text-white px-5 ">Github</button>
+              <Link href="https://github.com/Priyanka-Das-Dipa/techonology_and_electronics">
+                <button className="border text-white px-5 py-1 rounded-md">
+                  Client Side
+                </button>
+              </Link>
+              <Link href="https://github.com/Priyanka-Das-Dipa/techonology_and_electronics_server">
+                <button className="border text-white px-5 py-1 rounded-md">
+                  Server Site
+                </button>
               </Link>
             </div>
           </div>
